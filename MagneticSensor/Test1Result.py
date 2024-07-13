3cb66dfc76323e1316414039cd53b396a4718b86
Woohyuck-Jeoong <jwhyukk10@gmail.com>
2024-07-14 00:22:18 +0900

    add sys.platform

--- MagneticSensor/Test1Result.py
+++ MagneticSensor/Test1Result.py
@@ -17,93 +17,98 @@ def plotResult(minRow, maxRow, data, imgTitle):
     plotData = data.loc[rowList, :]
     plotData.plot(kind= "line", title= imgTitle)
 
-# Read raw data file names
-folderPath = input("폴더 경로: ")
-fileNames = sorted(glob.glob(folderPath + "*"))
+if sys.platform == "darwin":
 
-# Convert raw data to csv file
-for file in fileNames:
-    df = pd.read_table(file, names= ["col"])
-    df = df.iloc[:, 0].str[-39:]
-    df = pd.DataFrame(df, columns= ["col"])
+    # Read raw data file names
+    folderPath = input("폴더 경로: ")
+    fileNames = sorted(glob.glob(folderPath + "*"))
 
-    s1 = df["col"].str[4:9]
-    s2 = df["col"].str[9:14]
-    s3 = df["col"].str[14:19]
-    s4 = df["col"].str[20:25]
-    s5 = df["col"].str[25:30]
-    s6 = df["col"].str[30:35]
+    # Convert raw data to csv file
+    for file in fileNames:
+        df = pd.read_table(file, names= ["col"])
+        df = df.iloc[:, 0].str[-39:]
+        df = pd.DataFrame(df, columns= ["col"])
 
-    res = pd.concat((s1, s2), axis=1)
-    res = pd.concat((res, s3), axis=1)
-    res = pd.concat((res, s4), axis=1)
-    res = pd.concat((res, s5), axis=1)
-    res = pd.concat((res, s6), axis=1)
+        s1 = df["col"].str[4:9]
+        s2 = df["col"].str[9:14]
+        s3 = df["col"].str[14:19]
+        s4 = df["col"].str[20:25]
+        s5 = df["col"].str[25:30]
+        s6 = df["col"].str[30:35]
 
-    res.columns = ["s1", "s2", "s3", "s4", "s5", "s6"]
-    
-    saveFileName = str(file.split(f"{folderPath}")[1])
-    # res.to_csv(folderPath + saveFileName + ".csv", sep = ",")
+        res = pd.concat((s1, s2), axis=1)
+        res = pd.concat((res, s3), axis=1)
+        res = pd.concat((res, s4), axis=1)
+        res = pd.concat((res, s5), axis=1)
+        res = pd.concat((res, s6), axis=1)
 
-# Read csv files
-convertedFileList = sorted(glob.glob(folderPath + "*.csv"))
+        res.columns = ["s1", "s2", "s3", "s4", "s5", "s6"]
+        
+        saveFileName = str(file.split(f"{folderPath}")[1])
+        # res.to_csv(folderPath + saveFileName + ".csv", sep = ",")
 
-sensor1Data = pd.DataFrame()
-sensor2Data = pd.DataFrame()
-sensor3Data = pd.DataFrame()
-# sensor4Data = pd.DataFrame()
-# sensor5Data = pd.DataFrame()
-# sensor6Data = pd.DataFrame()
+    # Read csv files
+    convertedFileList = sorted(glob.glob(folderPath + "*.csv"))
 
-dfBaseMean = pd.DataFrame()
+    sensor1Data = pd.DataFrame()
+    sensor2Data = pd.DataFrame()
+    sensor3Data = pd.DataFrame()
+    # sensor4Data = pd.DataFrame()
+    # sensor5Data = pd.DataFrame()
+    # sensor6Data = pd.DataFrame()
 
-for file in convertedFileList:
-    name = file.split(f"{folderPath}")[1]
+    dfBaseMean = pd.DataFrame()
 
-    if name.startswith("s"):
-        distance = name.split('_')[1]
-        distance = distance.split(".")[0]
+    for file in convertedFileList:
+        name = file.split(f"{folderPath}")[1]
 
-        if name.split("_")[0] == "s1":
-            sensorData = pd.read_csv(file, sep= ",", usecols= [1], names = [f"d{distance}"], skiprows= [0])
-            sensor1Data = pd.concat((sensor1Data, sensorData), axis = 1)
-              
-        elif name.split("_")[0] == "s2":
-            sensorData = pd.read_csv(file, sep= ",", usecols= [2], names = [f"d{distance}"], skiprows= [0])
-            sensor2Data = pd.concat((sensor2Data, sensorData), axis = 1)
+        if name.startswith("s"):
+            distance = name.split('_')[1]
+            distance = distance.split(".")[0]
 
-        elif name.split("_")[0] == "s3":
-            sensorData = pd.read_csv(file, sep= ",", usecols= [3], names = [f"d{distance}"], skiprows= [0])
-            sensor3Data = pd.concat((sensor3Data, sensorData), axis = 1)
+            if name.split("_")[0] == "s1":
+                sensorData = pd.read_csv(file, sep= ",", usecols= [1], names = [f"d{distance}"], skiprows= [0])
+                sensor1Data = pd.concat((sensor1Data, sensorData), axis = 1)
+                
+            elif name.split("_")[0] == "s2":
+                sensorData = pd.read_csv(file, sep= ",", usecols= [2], names = [f"d{distance}"], skiprows= [0])
+                sensor2Data = pd.concat((sensor2Data, sensorData), axis = 1)
 
-        # elif name.split("_")[0] == "s4":
-        #     sensorData = pd.read_csv(file, sep= ",", usecols= [4], names = [f"d{distance}"], skiprows= [0])
-        #     sensor4Data = pd.concat((sensor4Data, sensorData), axis = 1)
+            elif name.split("_")[0] == "s3":
+                sensorData = pd.read_csv(file, sep= ",", usecols= [3], names = [f"d{distance}"], skiprows= [0])
+                sensor3Data = pd.concat((sensor3Data, sensorData), axis = 1)
 
-        # elif name.split("_")[0] == "s5":
-        #     sensorData = pd.read_csv(file, sep= ",", usecols= [5], names = [f"d{distance}"], skiprows= [0])
-        #     sensor5Data = pd.concat((sensor5Data, sensorData), axis = 1)
+            # elif name.split("_")[0] == "s4":
+            #     sensorData = pd.read_csv(file, sep= ",", usecols= [4], names = [f"d{distance}"], skiprows= [0])
+            #     sensor4Data = pd.concat((sensor4Data, sensorData), axis = 1)
 
-        # elif name.split("_")[0] == "s6":
-        #     sensorData = pd.read_csv(file, sep= ",", usecols= [6], names = [f"d{distance}"], skiprows= [0])
-        #     sensor6Data = pd.concat((sensor6Data, sensorData), axis = 1)
+            # elif name.split("_")[0] == "s5":
+            #     sensorData = pd.read_csv(file, sep= ",", usecols= [5], names = [f"d{distance}"], skiprows= [0])
+            #     sensor5Data = pd.concat((sensor5Data, sensorData), axis = 1)
 
-    elif name.startswith("Base"):
-        base = pd.read_csv(file, sep= ",", usecols= [1, 2, 3, 4, 5, 6])
-        baseMean = base.mean(axis = 0)
-        dfBaseMean = pd.concat((dfBaseMean, baseMean), axis= 1).T.to_numpy().reshape(6,)
+            # elif name.split("_")[0] == "s6":
+            #     sensorData = pd.read_csv(file, sep= ",", usecols= [6], names = [f"d{distance}"], skiprows= [0])
+            #     sensor6Data = pd.concat((sensor6Data, sensorData), axis = 1)
 
-scaledSensor1 = sensor1Data - dfBaseMean[0]
-scaledSensor2 = sensor2Data - dfBaseMean[1]
-scaledSensor3 = sensor3Data - dfBaseMean[2]
-# scaledSensor4 = sensor4Data - dfBaseMean[3]
-# scaledSensor5 = sensor5Data - dfBaseMean[4]
-# scaledSensor6 = sensor6Data - dfBaseMean[5]
+        elif name.startswith("Base"):
+            base = pd.read_csv(file, sep= ",", usecols= [1, 2, 3, 4, 5, 6])
+            baseMean = base.mean(axis = 0)
+            dfBaseMean = pd.concat((dfBaseMean, baseMean), axis= 1).T.to_numpy().reshape(6,)
 
-plotResult(50, 1000, scaledSensor1, "Sensor 1")
-plotResult(50, 1000, scaledSensor2, "Sensor 2")
-plotResult(50, 1000, scaledSensor3, "Sensor 3")
-# plotResult(50, 1000, scaledSensor4, "Sensor 4")
-# plotResult(50, 1000, scaledSensor5, "Sensor 5")
-# plotResult(50, 1000, scaledSensor6, "Sensor 6")
-plt.show()
+    scaledSensor1 = sensor1Data - dfBaseMean[0]
+    scaledSensor2 = sensor2Data - dfBaseMean[1]
+    scaledSensor3 = sensor3Data - dfBaseMean[2]
+    # scaledSensor4 = sensor4Data - dfBaseMean[3]
+    # scaledSensor5 = sensor5Data - dfBaseMean[4]
+    # scaledSensor6 = sensor6Data - dfBaseMean[5]
+
+    plotResult(50, 1000, scaledSensor1, "Sensor 1")
+    plotResult(50, 1000, scaledSensor2, "Sensor 2")
+    plotResult(50, 1000, scaledSensor3, "Sensor 3")
+    # plotResult(50, 1000, scaledSensor4, "Sensor 4")
+    # plotResult(50, 1000, scaledSensor5, "Sensor 5")
+    # plotResult(50, 1000, scaledSensor6, "Sensor 6")
+    plt.show()
+
+else:
+    print("Pleas run in mac")
---
+++ TestCode.py
@@ -0,0 +1,17 @@
+import platform
+
+# # print(platform.system())
+
+# if platform.system() == "Darwin":
+#     print("Macbook")
+# else:
+#     print("Window")
+
+import sys
+
+# print(sys.platform)
+
+if sys.platform == "darwin":
+    print("Mac")
+else:
+    print("Window")
