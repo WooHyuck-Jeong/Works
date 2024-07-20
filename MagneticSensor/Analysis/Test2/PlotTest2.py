c84171d6a6b42470af0f3abee65869ed89991b3c
Woohyuck-Jeoong <jwhyukk10@gmail.com>
2024-07-20 21:14:13 +0900

    자잘한 수정

--- MagneticSensor/Analysis/Test2/PlotTest2.py
+++ MagneticSensor/Analysis/Test2/PlotTest2.py
@@ -61,7 +61,7 @@ scaledCase = [scaledCase1, scaledCase2, scaledCase3, scaledCase4, scaledCase5,
               scaledCase11, scaledCase12, scaledCase13, scaledCase14, scaledCase15]
 
 # for i in range(len(scaledCase)):
-    # scaledCase[i].to_csv(f"/Users/jwh/Desktop/0720/MagneticTest/Test2/result/ScaledCase{i}.csv", sep= ",")
+#     scaledCase[i].to_csv(f"/Users/jwh/Desktop/0720/MagneticTest/Test2/result/ScaledCase{i+1}.csv", sep= ",")
 
 titleList = [f"Case {i+1}" for i in range(15)]
 
