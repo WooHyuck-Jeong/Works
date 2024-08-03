b3faa0eb3315dcee8d41d3a3138f2269e5264e14
Woohyuck-Jeoong <jwhyukk10@gmail.com>
2024-08-03 12:47:39 +0900

    Test2 Result Plot 코드 최적화

--- MagneticSensor/Test2/Test2MeanPlot.py
+++ MagneticSensor/Test2/Test2MeanPlot.py
@@ -3,25 +3,23 @@ import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
-# Data path
-dataPath = "/Users/jwh/Documents/Code/KOTC/MagneticSensor/Test0719_ReAnalysis/Test2Result/"
-
+# Datapath
+dataPath = "/Users/jwh/Documents/KOTC/수중도킹/2024년도/6_자기장센서 시험/Test0730_전달/Test2/result/"
 
 # Files
 fileNames = [file for file in sorted(glob.glob(dataPath + "*.csv")) if os.path.isfile(file)]
+
+base = pd.read_csv(fileNames[0], sep= ",")
+meanBase = pd.DataFrame(base.mean(axis= 0))
 
 fileIdx = [1, 8, 9, 10, 11, 12, 13, 14, 15, 2, 3, 4, 5, 6, 7]
 
 files = []
 
-for i in fileIdx:
-    caseData = pd.read_csv(fileNames[i], sep= ",")
+for idx in fileIdx:
+    caseData = pd.read_csv(fileNames[idx], sep= ",")
     files.append(caseData)
-
-base = pd.read_csv(fileNames[0], sep= ",")
-meanBase = pd.DataFrame(base.mean(axis= 0))
 
 sensorNames = [f's{i}' for i in np.arange(1, 7, 1)]
 
@@ -38,16 +36,16 @@ for i in range(len(files)):
     scaled = getScaledData(files[i])
     scaledData.append(scaled)
 
-meanScaled = [scaledData[i].mean(axis= 0) for i in range(len(scaledData))]
+yMin = -2200
+yMax = 2200
+yLim = [yMin, yMax]
 
 fig, axs = plt.subplots(3, 5, figsize= (20, 18))
-
-cmap = cm.get_cmap('tab20', 15)
-colors = [cmap(i) for i in range(15)]
-
 for i, ax in enumerate(axs.flat):
-    for sensor in sensorNames:
-        meanScaled[i].plot(kind="line", ax=ax, title=f'Case {i+1}', grid=True, marker="*", color=colors[i])
-
+    scaledData[i].plot(kind= "line",
+                       ax= ax,
+                       ylim= yLim,
+                       title= f"Case {i+1}",
+                       ).legend()
 plt.subplots_adjust(hspace= 0.4)
 plt.show()
